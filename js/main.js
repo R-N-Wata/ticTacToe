6610214b89d5175b4151ae8654521c3f8480c073
Ruth Wata <ruthnwata@gmail.com>
2022-05-08 23:09:37 -0500

    changes to js

--- js/main.js
+++ js/main.js
@@ -6,23 +6,36 @@
 
 let cells = ['c0', 'c1', 'c2', 'c3', 'c4', 'c5', 'c6', 'c7', 'c8']
 
+const box0 =document.querySelector('.box0')
+
+const box1 =document.querySelector('.box1')
+
+const box2 =document.querySelector('.box2')
+
+const box3 =document.querySelector('.box3')
+
+const box4 =document.querySelector('.box4')
+
+const box5 =document.querySelector('.box5')
+
+const box6 =document.querySelector('.box6')
+
+const box7 =document.querySelector('.box7')
+
+const box8 =document.querySelector('.box8')
+
+const h2 = document.querySelector('h2')
+
+const winner = document.querySelector('.winner')
+
 let buttons =  [...document.querySelectorAll('button')]
 
 const keys = document.querySelectorAll('button').forEach((btn, i)=> {
     btn.addEventListener('click', function (){
-
-     
         
-           
-
-        
-        ticTacToe.winner(btn, i)
+        ticTacToe.winner(btn)
     })
 })
-
-
-
-
 
 
 const ticTacToe = {
@@ -30,16 +43,21 @@ const ticTacToe = {
 
     computer: 'O',
 
-    winner(btn, i){
+    winner(btn){
 
+        if(box0.innerText === 'X' && box1.innerText === 'X' && box2.innnerText === 'X'){
 
-        
-     
-        ticTacToe.computerChoice(btn)
+            h2.innerText = `You've won!`
+
+            winner.classList.remove('hidden')
+            console.log('hi')
+
+        }else {
+            ticTacToe.computerChoice(btn)
         ticTacToe.showPlayerChoice(btn)
-
         buttons = buttons.filter(b => cells.includes(b.value) )
         buttons.forEach(b => console.log(b.value))
+        }
 
     },
 
@@ -54,11 +72,7 @@ console.log(random )
             
         }
              this.showComputer(random)
-             cells.splice(random,1)
-         
-        
-       
-         
+             cells.splice(random,1) 
         
     },
     showPlayerChoice(btn){
