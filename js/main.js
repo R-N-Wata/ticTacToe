2639be29c007e25c03956f42e5ad134f78d0947d
Ruth Wata <ruthnwata@gmail.com>
2022-05-10 08:27:29 -0500

    added comments

--- js/main.js
+++ js/main.js
@@ -56,13 +56,22 @@ const ticTacToe = {
 
     winner(btn){
 
-        let player1Wins = possibleWins.some(out => out.every(el =>{ 
+        //finds the winner if possible wins are met
+        let player1Wins = possibleWins.some(out => out.every(el =>{  
+            let answer = false
             for(let i = 0; i < buttons.length; i++){
-                if(buttons[i].value === el && buttons[i].innerText === this.player){
-                  console.log(true)
+
+               
+                if(buttons[i].value == el){
+                  answer = true
+                }else{
+                    answer = false
                 }
-                console.log(false)
-            } }
+            }
+            console.log(answer)
+            return answer
+        
+        }
             )
        
         )
@@ -76,9 +85,9 @@ const ticTacToe = {
             console.log('hi')
 
         }
-        // winner.classList.add('hidden')
         
-           
+        
+        // removes the cell already taken
         buttons = buttons.filter(b => cells.includes(b.value) )
        
         
@@ -87,9 +96,11 @@ const ticTacToe = {
 
     computerChoice(btn){
 
+        //generates a computer random cell
         let len = cells.length -2
         let random = Math.round(Math.random() * len)
 
+        //if cell is already taken, checks for another random number
         while(cells[random] === btn.value){
             
             random = Math.round(Math.random() * len)
@@ -100,7 +111,8 @@ const ticTacToe = {
         
     },
     showPlayerChoice(btn){
-     
+    
+        //places 'X' in player cell
         btn.innerText = this.player
 
         cells.splice(cells.indexOf(btn.value), 1)
@@ -109,6 +121,7 @@ const ticTacToe = {
 
     showComputer(cell){
 
+        //places 'O' in computer cell
         buttons.forEach((b, i) => {
             if( i === cell){
                 b.innerText = this.computer
